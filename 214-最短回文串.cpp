f7f8d44abe81b08946eb845392711213954c1236
Perry961002 <37168176+Perry961002@users.noreply.github.com>
2018-09-01 11:30:43 +0800

    Update 214-最短回文串.cpp

---
+++ 214-最短回文串.cpp
@@ -0,0 +1,25 @@
+static const auto __ = []() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    return nullptr;
+}();
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+        //KMP算法
+        string str = s;
+        reverse(str.begin(), str.end());
+        str = s+ "#" + str;//添加冗余字符
+        int len1 = s.size(), len2 = str.size();
+        vector<int> vec(len2, 0);
+        for(int i = 1; i < len2; i++)
+        {
+            int k = vec[i-1];
+            while(k > 0 && str[k] != str[i]) 
+                k = vec[k-1];
+            vec[i] = (k += str[i] == str[k]);
+        }
+        return str.substr(len1+1, len1-vec[len2-1]) + s;
+
+    }
+};
