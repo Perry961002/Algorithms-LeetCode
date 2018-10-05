6c06b281e6741ab148725973bf61b8b20b98b367
Perry961002 <Perry961002@163.com>
2018-10-05 19:13:59 +0800

    分治, 归并

--- 023-合并K个排序链表.cpp
+++ 023-合并K个排序链表.cpp
@@ -37,12 +37,22 @@ public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         if(lists.empty())
             return NULL;
+        /**
+        *
         while(lists.size() > 1){
             ListNode* l1 = lists.back();
             lists.pop_back();
             ListNode* l2 = lists.back();
             lists.pop_back();
             lists.push_back(mergeTwoLists(l1, l2));
+        }
+        */
+        //以上代码分析之后发现会有大量的对重复节点的操作,需要把插入和删除分在两端进行,所以做出下面的优化
+        while(lists.size() > 1){
+            //合并后在尾端插入,前端删除
+            lists.push_back(mergeTwoLists(lists[0], lists[1]));
+            lists.erase(lists.begin());
+            lists.erase(lists.begin());
         }
         return lists.back();
     }
